2b37f42db3afdfcfe7d504d4c2e9293f42d94e5a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-04-04 18:13:58 -0300

    project init

---
+++ src/main/java/com/piradrian/clientinvoice/ClientInvoiceApplication.java
@@ -0,0 +1,11 @@
+package com.piradrian.clientinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientInvoiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ClientInvoiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/piradrian/clientinvoice/ClientInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piradrian.clientinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
